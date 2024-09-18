c6fb4fa07ed904c67ce89f94aac723a4cc313b11
Derek J <derekjiang25@gmail.com>
2024-09-18 11:37:55 +1000

    added list ends

---
+++ 12. list-ends.py
@@ -0,0 +1,10 @@
+
+def firstAndLast(list):
+    return [list[0], list[len(list) - 1]];
+    
+list = [5, 10, 15, 20, 25];
+# list = [5, 10];
+# list = [5];
+# list = [];
+    
+print(firstAndLast(list));
